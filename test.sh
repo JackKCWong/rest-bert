75ac3c397879acbbb98b90e5da347f3c382ccc90
JackKCWong <jiajue.huang@live.com>
2023-10-22 09:17:04 +0800

    add env

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+curl http://localhost:8080/v1/embeddings \
+  -H "Content-Type: application/json" \
+  -d '{
+    "input": ["The food was delicious and the waiter"],
+    "model": "all-MiniLM-L12-v2"
+  }'
+
